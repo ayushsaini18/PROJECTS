44d032185981db2da6391415ba8419f207e0c664
ayushsaini18 <100209722+ayushsaini18@users.noreply.github.com>
2024-05-16 14:58:24 +0530

    updated

--- BharatClockVersion/ClockVersion/src/Components/ClockHeading.jsx
+++ BharatClockVersion/ClockVersion/src/Components/ClockHeading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ClockHeading() {
   return (
     <div>
-        <center><h1>BHARAT CLOCK</h1></center>
+        <center><h1>BHARAT CLOCK.</h1></center>
     </div>
   )
 }
